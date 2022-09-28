403df0327e2f95f6b6860601ae00569f44465bb1
Sterbweise <63killian63@gmail.com>
2022-09-28 09:58:55 +0200

    Update

--- install.sh
+++ install.sh
@@ -1,87 +1,183 @@
 #!/bin/bash
-## Update Region
-echo -ne "[1/8] Set last version of your system [####                     ] (13%)                      \r"
-{
-apt update && apt upgrade -y
-} > /dev/null 2>&1
-## End Region
+clear
 
-## Firewall Region
-echo -ne "[2/8] Install firewall and allow 22 port [#######                  ] (25%)                   \r"
+# Sudoer permission
+if [[ $UID != 0 ]]; then
+    echo "Please run this script with sudo:"
+    echo "sudo env "HOME='$HOME'" $0 $*"
+    exit 1
+fi
+
+# Colors Section
+YELLOW='\033[1;33m'
+GREY='\033[1;37m'
+PURPUL='\033[0;35m'
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+RED='\033[0;31m'
+NC='\033[0m'
+
+
+# Function Progress
+Spinner (){ 
+pid=$!
+spin='-\|/'
+i=0
+while kill -0 $pid 2>/dev/null
+do
+  i=$(( (i+1) %4 ))
+  printf "\r [${spin:$i:1}] $1"
+  sleep .1
+done
+printf "\r [${GREEN}\xE2\x9C\x94${NC}] $1 \n"
+}
+
+# Home Section
+logo(){
+printf "${RED}
+  _______ _____    _____                            _____           _        _ _           
+ |__   __| ____|  / ____|                          |_   _|         | |      | | |          
+    | |  | |__   | (___   ___ _ ____   _____ _ __    | |  _ __  ___| |_ __ _| | | ___ _ __ 
+    | |  |___ \   \___ \ / _ \ \'__\ \ / / _ \ \'__|   | | | \'_ \/ __| __/ _\` | | |/ _ \ \'__|
+    | |   ___) |  ____) |  __/ |   \ V /  __/ |     _| |_| | | \__ \ || (_| | | |  __/ |   
+    |_|  |____/  |_____/ \___|_|    \_/ \___|_|    |_____|_| |_|___/\__\__,_|_|_|\___|_|   
+                                                                                           
+                                                                                           
+ ${NC}                                                                                        
+                         ╔══════════════════════════════╗
+                         ║      Made by ${BLUE}Sterbweise${NC}      ║
+                         ╠══════════════════════════════╣
+                         ║ ${PURPUL}\e]8;;https://github.com/Sterbweise\e\\Github\e]8;;\e\\\\${NC} | ${RED}\e]8;;https://www.youtube.com/channel/UCRWfp6bi0-wlhaRe2YQ2dwQ\e\\Youtube\e]8;;\e\\\\${NC} | ${GREY}\e]8;;https://forum.plutonium.pw/user/minami\e\\Plutonium\e]8;;\e\\\\${NC} ║
+                         ╚══════════════════════════════╝ \n \n"
+}
+
+logo
+
+# Languages Selection
+printf "
+${YELLOW}Select your languages : ${NC}
+[0] English
+[1] French
+\n
+"
+read -p '>>> ' languages
+clear
+logo
+
+# Choices Section
+mfirewall=('Do you want install UFW firewall (Y/n) ?' 'Voulez-vous installer le pare-feu UFW (O/n) ?')
+printf "${YELLOW}${mfirewall[$languages]}${NC}\n"
+read -p '>>> ' firewall
+
+mdotnet=('Do you want install Dotnet [Required for IW4Madmin] (Y/n) ?' 'Voulez-vous installer Dotnet [Requis pour IW4Madmin] (O/n) ?')
+printf "\n\n${YELLOW}${mdotnet[$languages]}${NC}\n"
+read -p '>>> ' dotnet
+stty igncr
+clear
+logo
+
+# Update Systeme
+mupdate=('Updating the system' 'Mise a jours du systeme')
 {
-apt install ufw fail2ban -y && \
-ufw allow 22/tcp && \
-ufw default allow outgoing && \
-ufw default deny incoming && \
-ufw -f enable
-} > /dev/null 2>&1
-## End Region
+apt update
+} > /dev/null 2>&1 &
+Spinner "${mupdate[$languages]}"
+
+# Setup Firewall
+if [ "$firewall" = 'y' ] || [ "$firewall" = '' ] || [ "$firewall" = 'Y' ] || [ "$firewall" = 'o' ] || [ "$firewall" = 'O' ] ; then
+  mfirewall2=('Firewall installation and ssh port opening.' 'Installation du pare-feu et ouverture du port ssh.') 
+  {
+    apt install ufw fail2ban -y && \
+    ufw allow 22/tcp && \
+    ufw default allow outgoing && \
+    ufw default deny incoming && \
+    ufw -f enable
+  } > /dev/null 2>&1 &
+  Spinner "${mfirewall2[$languages]}"
+fi
 
 # Enable 32 bit packages
-echo -ne "[3/8] Enable 32 bit packages [###########              ] (38%)                              \r"
+mbit=('Enabling 32-bit packages' 'Activation des paquets 32 bits')
 {
-dpkg --add-architecture i386 && \
-apt-get update -y && \
-apt-get install wget gnupg2 software-properties-common apt-transport-https curl transmission-cli -y
-} > /dev/null 2>&1
+  dpkg --add-architecture i386 && \
+  apt-get update -y && \
+  apt-get install wget gnupg2 software-properties-common apt-transport-https curl transmission-cli -y
+} > /dev/null 2>&1 &
+Spinner "${mbit[$languages]}"
 
-## Wine Region
-echo -ne "[4/8] Installing Wine [##############           ] (50%)                                    \r"
+# Installing Wine
+mwine=('Installing Wine.' 'Installation de Wine.')
 {
-wget -nc https://dl.winehq.org/wine-builds/winehq.key
-apt-key add winehq.key && \
-apt-add-repository 'deb https://dl.winehq.org/wine-builds/debian/ buster main'
-rm winehq.key
-apt update -y
-apt install --install-recommends winehq-stable -y
+  wget -nc https://dl.winehq.org/wine-builds/winehq.key
+  apt-key add winehq.key && \
+  apt-add-repository 'deb https://dl.winehq.org/wine-builds/debian/ buster main'
+  rm winehq.key
+  apt update -y
+  apt install --install-recommends winehq-stable -y
 
-# Add Variables to the environment at the end of ~/.bashrc
-echo -e 'export WINEPREFIX=~/.wine\nexport WINEDEBUG=fixme-all\nexport WINEARCH=win64' >> ~/.bashrc
-echo -e 'export DISPLAY=:0' >> ~/.bashrc
-source ~/.bashrc
-winecfg
-} > /dev/null 2>&1
-## End Region
+  # Add Variables to the environment at the end of ~/.bashrc
+  echo -e 'export WINEPREFIX=~/.wine\nexport WINEDEBUG=fixme-all\nexport WINEARCH=win64' >> ~/.bashrc
+  echo -e 'export DISPLAY=:0' >> ~/.bashrc
+  source ~/.bashrc
+  winecfg
+} > /dev/null 2>&1 &
+Spinner "${mwine[$languages]}"
 
-## Pre-Required for IW4MAdmin Region
-echo -ne "[5/8] Installing Pre-Required for IW4MAdmin [#################        ] (63%)            \r"
+# Dotnet Installation
+if [ $dotnet == 'y' ] || [ $dotnet == '' ] || [ $dotnet == 'Y' ] || [ $dotnet == 'o' ] || [ $dotnet == 'O' ] ; then
+  mdotnet2=('Installing Dotnet.' 'Installation de Dotnet.')
+  {
+    #Dotnet Package
+    wget https://packages.microsoft.com/config/debian/10/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+    sudo dpkg -i packages-microsoft-prod.deb
+    rm packages-microsoft-prod.deb
+
+    #Install the SDK
+    apt-get install -y dotnet-sdk-3.1
+    apt-get install -y dotnet-sdk-6.0
+
+    #Install the runtime
+    apt-get install -y aspnetcore-runtime-3.1
+	  apt-get install -y aspnetcore-runtime-6.0
+  } > /dev/null 2>&1 &
+  Spinner "${mdotnet2[$languages]}"
+fi
+
+mbinary=('Game Binary Installation.' 'Installation des fichiers binaires.')
 {
-#Installation .NET Core 3.1
-wget https://packages.microsoft.com/config/debian/10/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
-sudo dpkg -i packages-microsoft-prod.deb
-rm packages-microsoft-prod.deb
+    # Download plutonium-updater
+    cd $HOME/T5Server/Plutonium/
+    wget https://github.com/mxve/plutonium-updater.rs/releases/latest/download/plutonium-updater-x86_64-unknown-linux-gnu.tar.gz
+    tar xfv plutonium-updater-x86_64-unknown-linux-gnu.tar.gz
+    rm plutonium-updater-x86_64-unknown-linux-gnu.tar.gz
+    chmod +x plutonium-updater
 
-#Install the SDK
-#The .NET SDK allows you to develop apps with .NET. If you install the .NET SDK, you don't need to install the corresponding runtime. To install the .NET SDK, run the following commands:
+    # Make executable script
+    chmod +x $HOME/T5Server/Plutonium/T5Server.sh
+    chmod +x $HOME/T5Server/Plutonium/T5_mp_server.sh
+    chmod +x $HOME/T5Server/Plutonium/T5_zm_server.sh
 
-apt-get update; \
-	apt-get install -y dotnet-sdk-3.1
-	apt-get install -y dotnet-sdk-6.0
+    # Download Game File
+    cd $HOME/T5Server/
+    wget https://plutonium.pw/pluto_t5_full_game.torrent
+    tmpfile=$(mktemp)
+    chmod a+x $tmpfile
+    echo "killall transmission-cli" > $tmpfile
+    transmission-cli -f $tmpfile pluto_t5_full_game.torrent -w $HOME/T5Server
 
-#Install the runtime
-#The ASP.NET Core Runtime allows you to run apps that were made with .NET that didn't provide the runtime. The following commands install the ASP.NET Core Runtime, which is the most compatible runtime for .NET. In your terminal, run the following commands:
+    # Clean Installation
+    rm $HOME/T5Server/pluto_t5_full_game.torrent
+    mv $HOME/T5Server/pluto_t5_full_game $HOME/T5Server/Server
+    rm -r $HOME/T5Server/Server/redist
+    rm $HOME/T5Server/README.md
 
-apt-get update; \
-	apt-get install -y aspnetcore-runtime-3.1
-	apt-get install -y aspnetcore-runtime-6.0
-} > /dev/null 2>&1
-## End Region
+} > /dev/null 2>&1 &
+  Spinner "${mbinary[$languages]}"
 
-echo -ne "[6/8] Game Binary Installation [####################     ] (75%)                      \r"
-{
-wget https://github.com/mxve/plutonium-updater.rs/releases/latest/download/plutonium-updater-x86_64-unknown-linux-gnu.tar.gz
-tar xfv plutonium-updater-x86_64-unknown-linux-gnu.tar.gz
-rm plutonium-updater-x86_64-unknown-linux-gnu.tar.gz
-chmod +x plutonium-updater
-mv $HOME/T5Server/plutonium-updater $HOME/T5Server/Plutonium
-} > /dev/null 2>&1
-
-echo -ne "[7/8] Download Torrent Game File [#######################  ] (88%)                    \r"
-{
-wget https://plutonium.pw/pluto_t5_full_game.torrent
-chmod +x ~/T5Server/Plutonium/T5_mp_server.sh
-chmod +x ~/T5Server/Plutonium/T5_zm_server.sh
-} > /dev/null 2>&1
-
-echo -ne "[8/8] Installation Complete [#########################] (100%)                       \r"
-echo ""
+mfinish=('Installation finished.' 'Installation terminee.')
+mquit=('ENTER to quit.' 'ENTER pour quitter.')
+printf "\n${GREEN}${mfinish[$languages]}${NC}\n"
+printf "\n${mquit[$languages]}"
+stty -igncr
+read
+exit
